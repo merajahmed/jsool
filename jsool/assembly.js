5cf707cd30307a8145b920c6bda9c9a2d7ad9241
mikhas.rock <mikhas.rock@65f8f30c-6cf0-11de-b0b6-0bd851037a78>
2009-08-19 18:05:05 +0000

    My Assembly Experience - Faster compiler. - Easyer to develop new instructions

--- jsool/assembly.js
+++ jsool/assembly.js
@@ -1,3 +1,5 @@
+//TODO closure it after devlopment
+//Manual intel: http://www.intel.com/products/processor/manuals/index.htm
 (function(){
 	window.assembly = {};
 	var self = window.assembly; 
@@ -10,51 +12,88 @@
 	var hexaToNum = {};
 	var numToHexa = [];
 	var memory = [];
+	var interruptions = {};
+	self.memory = memory;
 	
+	//Basic register
 	function Register(){this.value = '0000';}
 	Register.prototype = new Object();
 	Register.prototype.get = function(){return this.value;};
 	Register.prototype.set = function(val){this.value = val;};
-	
-	function ProgramStackRegister(){}
-	ProgramStackRegister.prototype = new Register();
-	ProgramStackRegister.prototype.step = function(){this.value = numToHexa[hexaToNum[this.value] + 1];};
-	
+	//Segment register
+	function SegmentRegister(){}
+	SegmentRegister.prototype = new Register();
+	SegmentRegister.prototype.step = function(){this.value = numToHexa[hexaToNum[this.value] + 1];};
+	SegmentRegister.prototype.stepBack = function(){this.value = numToHexa[hexaToNum[this.value] - 1];};
+	//Lower part register
 	function LowerRegister(parent){this.parent = parent;}
 	LowerRegister.prototype = new Register();
-	LowerRegister.prototype.get = function(){return this.parent.value.substring(0,2);};
-	LowerRegister.prototype.set = function(val){this.parent.value = val + this.parent.value.substring(2,4);};
-	
+	LowerRegister.prototype.get = function(){return '00'+this.parent.value.substring(2,4);};
+	LowerRegister.prototype.set = function(val){this.parent.value = this.parent.value.substring(0,2) + val.substring(2,4);};
+	//Higher part register
 	function HigherRegister(parent){this.parent = parent;}
 	HigherRegister.prototype = new Register();
-	HigherRegister.prototype.get = function(){return this.parent.value.substring(2,4);};
-	HigherRegister.prototype.set = function(val){this.parent.value = this.parent.value.substring(0,2) + val;};
+	HigherRegister.prototype.get = function(){return this.parent.value.substring(0,2)+'00';};
+	HigherRegister.prototype.set = function(val){this.parent.value = val.substring(0,2) + this.parent.value.substring(2,4);};
 	
+	//Processor flags
+	var flags = {
+		CF: false, //Carry Flag - becomes one if an +, -, AND, OR, results in a value larger than a register
+		PF: false, //Parity Flag - becomes 1 if the lower 8-bits of an operation contains an even number of 1 bits
+		AF: false, //Auxiliary Flag - Set on a carry or borrow to the value of the loer order 4 bits
+		ZF: false, //Zero Flag - becomes 1 if an operation results in a 0 writeback, or 0 register
+		SF: false, //Sign Flag - is 1 if the value saved is negative, 0 for positive
+		TF: false, //Trap Flag - allows for the stopping of code within a segment
+		IF: false, //Interrupt Flag - when this flag is set, the processor begins ‘listening’ for external interupts
+		DF: false, //Direction Flag - determines the direction to move through the code
+		OF: false  //Overflow Flag - becomes 1 if the operation is larger than available space to write
+	};
+	
+	//Create the registers
 	var registers = (function(){
-		AX = new Register();
-		BX = new Register();
-		CX = new Register();
-		DX = new Register();
-		PS = new ProgramStackRegister();//Program stack
-		AL = new LowerRegister(AX);
-		AH = new HigherRegister(AX);
-		BL = new LowerRegister(BX);
-		BH = new HigherRegister(BX);
-		CL = new LowerRegister(CX);
-		CH = new HigherRegister(CX);
-		DL = new LowerRegister(DX);
-		DH = new HigherRegister(DX);
+		var AX = new Register();//Accumulator: aritimetica e I/O
+		var BX = new Register();//Base: base ou ponteiro
+		var CX = new Register();//Counter: contador
+		var DX = new Register();//Displacement: uso geral
+		
+		var AL = new LowerRegister(AX);
+		var AH = new HigherRegister(AX);
+		var BL = new LowerRegister(BX);
+		var BH = new HigherRegister(BX);
+		var CL = new LowerRegister(CX);
+		var CH = new HigherRegister(CX);
+		var DL = new LowerRegister(DX);
+		var DH = new HigherRegister(DX);
+		
+		var CS = new SegmentRegister();//Code Segment: codigo sendo executado
+		var DS = new SegmentRegister();//Data Segment: endereco dos dados do programa
+		var ES = new SegmentRegister();//Extra Segment: para utilização do programador
+		var SS = new SegmentRegister();//Stack Segment: endereço da pilha
+		
+		var SI = new Register();//Source Index: utilizado para mover dados
+		var DI = new Register();//Destiny Index: utilizado para mover dados
+		
+		var BP = new Register();//Base Pointer: argumentos de funcoes
+		var SP = new Register();//Stack Pointer: topo da pilha
+		
 		return {
 			AX:AX,BX:BX,CX:CX,DX:DX,
 			AL:AL,AH:AH,BL:BL,BH:BH,
 			CL:CL,CH:CH,DL:DL,DH:DH,
-			PS:PS
+			CS:CS,DS:DS,ES:ES,SS:SS,
+			SI:SI,DI:DI,BP:BP,SP:SP
 		};
 	})();
-	
-	/**
-	 * Speed up convertions between numbers and hexas
-	 */
+	function printRegisters(){
+		var b = [''];
+		var c = 0;
+		for(var r in registers){
+			c++;
+			b.push(r+':'+registers[r].get()+(c%4 == 0?'\n':''));
+		}
+		console.debug(b.join(' '));
+	};
+	//Speed up convertions between numbers and hexas
 	(function speedUpConvertions(){
 		var hexaNumbers = [];
 		for(var i = 0; i < 16; i++){
@@ -70,251 +109,339 @@
 			}
 	})();
 	
-	/**
-	 * Checks if an expression is a pure hexa value
-	 */
-	function isValue(expression){
-		return expression.length == 4 && expression.match(/^[0-9ABCEDF]{4}$/) != null;
+	
+	//Checks if an expression is a pure hexa value
+	function isValue(expression){return expression.length == 4 && expression.match(/^[0-9ABCEDF]{4}$/) != null;}	
+	//Checks if an expression is a memory adress
+	function isMemory(expression){return expression.length == 6 && expression.match(/^\[([0-9ABCDEF]{4})]$/) != null;}
+	//Checks if an expression is a register
+	function isRegister(expression){return expression.length == 2 && (expression in registers);}
+	//Checks if an expression is a flag
+	function isFlag(expression){return expression.length == 2 && (expression in flags);}
+	//Checks if an expression is a Reference
+	function isReference(expression){
+		if(!expression.match(/^\[([A-Z]{2})]$/)){
+			return false;
+		}
+		var ref = expression.replace(/^\[([A-Z]{2})]$/,'$1');
+		return (ref in registers) || (ref in flags);
 	}
-	/**
-	 * Checks if an expression is a memory adress
-	 */
-	function isMemory(expression){
-		return expression.length == 6 && expression.match(/^\[([0-9ABCDEF]{4})]$/) != null;
+	//Removes from a memory adress pattern [AAAA] the square brackets
+	function getMemoryAdress(expression){return expression.replace(/^\[([0-9ABCDEF]{4})]$/,'$1');}
+	// Remover extra spaces on command line
+	function ajustCodeLine(expression){return expression.trim().replace(/[\s]{0,1}[,][\s]{0,1}/,',').replace(/[\s]+/,' ');}
+	
+	function getTypeOf(expression){
+		if(isMemory(expression)) return 'MEM';
+		if(isValue(expression)) return 'VAL';
+		if(isRegister(expression)) return 'REG';
+		if(isFlag(expression)) return 'FLA';
+		if(isReference(expression)) return 'REF';
+		return null;
 	}
-	/**
-	 * Checks if an expression is a register
-	 */
-	function isRegister(expression){
-		return expression.length == 2 && (expression in registers);
-	}
-	/**
-	 * Removes from a memory adress pattern [AAAA] the square brackets
-	 */
-	function getMemoryAdress(expression){
-		return expression.replace(/^\[([0-9ABCDEF]{4})]$/,'$1');
-	}
-	//Main compile function
+	//Better version of the compiler
 	function load(code, offset){
 		code = code.trim().toUpperCase();
 		if(code.length == 0) return;
-		console.profile('Compile');
 		try{
 			var memoryIndex = offset == null ? 0 : offset;
 			var lines = code.split('\n');
 			var line, compiled, cmd;
+			
 			for(var index = 0; index < lines.length; index++){
 				line = ajustCodeLine(lines[index]);
-				cmd = line.split(' ')[0];
-				compiled = compilers[cmd](line);
-				for(var i = 0; i < compiled.length; i++, memoryIndex++){
-					memory[memoryIndex] = compiled[i];
+				var splited = line.split(' ');
+				var args = splited[1].split(',');
+				
+				var inst = splited[0];
+				var arg;
+				
+				for(var a = 0; a < args.length; a++){
+					arg = args[a];
+					var type = getTypeOf(arg);
+					if(type == null) throw new Error('Invalid argument: '+arg);
+					if(type == 'MEM') args[a] = getMemoryAdress(arg);
+					inst = inst + '_' + type;
+				}
+				cmd = commands[inst];
+				
+				if(!cmd) throw Error("Unknown command: "+inst);
+				
+				memory[memoryIndex++] = cmd.CODE;
+				for(var i = 0; i < args.length; i++, memoryIndex++){
+					memory[memoryIndex] = args[i];
 				}
 			}
-		}catch(e){
-			console.profileEnd('Compile');
-			throw new Error("Compilation Error (line:"+index+")\n"+e);
-		}
-		console.profileEnd('Compile');
-	}
-	//Main run function
-	function run(){
-		console.profile("Run");
-		var PS = registers.PS;
-		var instruction;
-		PS.set(numToHexa[0]);
-		try{
-			while(true){
-				instruction = memory[hexaToNum[PS.get()]];
-				instructions[instruction]();
-				PS.step();
-			}
-		}catch(e){
 			
+		}catch(e){
+			throw new Error("Compilation Error (line:"+index+":\""+line+"\")\n"+e);
 		}
-		console.profileEnd("Run");
-		console.info("FINISHED AX:%s BX:%s CX:%s DX:%s", registers.AX.get(),registers.BX.get(),registers.CX.get(),registers.DX.get());
 	}
 	
-	function ajustCodeLine(expression){
-		return expression.trim().replace(/[\s]{0,1}[,][\s]{0,1}/,',').replace(/[\s]+/,' ');
+	//System bootstrap
+	function run(offset){
+		offset = offset || 0;
+		var CS = registers.CS;
+		CS.set(numToHexa[offset]);
+		var instruction = memory[hexaToNum[CS.get()]];
+		if(!instruction) throw 1;
+		instructions[instruction]();
+	}
+	
+	//Run next instruction on segment
+	function runNextInstruction(){
+		var CS = registers.CS;
+		CS.step();
+		var instruction = memory[hexaToNum[CS.get()]];
+		if(!instruction) throw 1;
+		instructions[instruction]();
 	}
 
 	self.reset = function(){memory = [];};
 	
-	/**
-	 * All registered commands with all its informations
-	 */
+	//All registered commands with all its informations
 	var cmdCounter = 0;
+	//Returns the next value on the Code Segment
+	function getNextCommand(){
+		registers.CS.step();
+		return memory[hexaToNum[registers.CS.get()]];
+	}
 	commands = {
-		MOV_VAL_TO_MEM:{
+		//INT instruction
+		INT_VAL:{
 			CODE:numToHexa[cmdCounter++],
-			FUNCTION: function (){
-				registers.PS.step();
-				var to = memory[hexaToNum[registers.PS.get()]];
-				registers.PS.step();
-				var from = memory[hexaToNum[registers.PS.get()]];
+			FUNCTION: function(){
+				var interruption = getNextCommand();
+				interruptions[interruption]();
+			}
+		},
+		//MOV [XXXX], XXXX
+		MOV_MEM_VAL:{
+			CODE:numToHexa[cmdCounter++],
+			FUNCTION: function (){				
+				var to = getNextCommand();
+				var from = getNextCommand();
 				memory[hexaToNum[to]] = from;
+				runNextInstruction();
 			}
 		},
-		MOV_REG_TO_MEM:{
+		//MOV [XXXX], AX
+		MOV_MEM_REG:{
 			CODE:numToHexa[cmdCounter++],
 			FUNCTION:function (){
-				registers.PS.step();
-				var to = memory[hexaToNum[registers.PS.get()]];
-				registers.PS.step();
-				var from = memory[hexaToNum[registers.PS.get()]];
-				memory[hexaToNum[to]] = registers[from];
+				var to = getNextCommand();
+				var from = getNextCommand();
+				memory[hexaToNum[to]] = registers[from].get();
+				runNextInstruction();
 			}
 		},
-		MOV_MEM_TO_MEM:{
+		//MOV [XXXX], [XXXX]
+		MOV_MEM_MEM:{
 			CODE:numToHexa[cmdCounter++],
-			FUNCTION:function MOV_MEM_TO_MEM(){
-				registers.PS.step();
-				var to = memory[hexaToNum[registers.PS.get()]];
-				registers.PS.step();
-				var from = memory[hexaToNum[registers.PS.get()]];
+			FUNCTION:function MOV_MEM_MEM(){
+				var to = getNextCommand();
+				var from = getNextCommand();
 				memory[hexaToNum[to]] = memory[hexaToNum[from]];
+				runNextInstruction();
 			}
 		},
-		MOV_VAL_TO_REG: {
+		//MOV AX, XXXX
+		MOV_REG_VAL: {
 			CODE:numToHexa[cmdCounter++],
 			FUNCTION:function (){
-				registers.PS.step();
-				var to = memory[hexaToNum[registers.PS.get()]];
-				registers.PS.step();
-				var from = memory[hexaToNum[registers.PS.get()]];
+				var to = getNextCommand();
+				var from = getNextCommand();
 				registers[to].set(from);
+				runNextInstruction();
 			}
 		},
-		MOV_REG_TO_REG:{
+		//MOV AX, AX
+		MOV_REG_REG:{
 			CODE:numToHexa[cmdCounter++],
 			FUNCTION:function (){
-				registers.PS.step();
-				var to = memory[hexaToNum[registers.PS.get()]];
-				registers.PS.step();
-				var from = memory[hexaToNum[registers.PS.get()]];
+				var to = getNextCommand();
+				var from = getNextCommand();
 				registers[to].set(registers[from].get());
+				runNextInstruction();
 			} 
 		},
-		MOV_MEM_TO_REG:{
+		//MOV AX, [XXXX]
+		MOV_REG_MEM:{
 			CODE:numToHexa[cmdCounter++],
 			FUNCTION:function (){
-				registers.PS.step();
-				var to = memory[hexaToNum[registers.PS.get()]];
-				registers.PS.step();
-				var from = memory[hexaToNum[registers.PS.get()]];
+				var to = getNextCommand();
+				var from = getNextCommand();
 				registers[to].set(memory[hexaToNum[from]]);
+				runNextInstruction();
 			} 
 		},
-		XCHG_MEM_TO_MEM:{
+		//XCHG [XXXX], [XXXX]
+		XCHG_MEM_MEM:{
 			CODE:numToHexa[cmdCounter++],
 			FUNCTION: function(){
 				var temp;
-				registers.PS.step();
-				var to = memory[hexaToNum[registers.PS.get()]];
-				registers.PS.step();
-				var from = memory[hexaToNum[registers.PS.get()]];
+				var to = getNextCommand();
+				var from = getNextCommand();
 				temp = memory[hexaToNum[to]]; 
 				memory[hexaToNum[to]] = memory[hexaToNum[from]];
 				memory[hexaToNum[from]] = temp;
+				runNextInstruction();
 			}
 		},
-		XCHG_MEM_TO_REG:{
+		//XCHG AX, [XXXX]
+		XCHG_REG_MEM:{
 			CODE:numToHexa[cmdCounter++],
 			FUNCTION: function(){
 				var temp;
-				registers.PS.step();
-				var to = memory[hexaToNum[registers.PS.get()]];
-				registers.PS.step();
-				var from = memory[hexaToNum[registers.PS.get()]];				
+				var to = getNextCommand();
+				var from = getNextCommand();				
 				temp = registers[from].get();
 				registers[from].set(memory[hexaToNum[to]]);
 				memory[hexaToNum[to]] = temp;
+				runNextInstruction();
 			}
 		},
-		XCHG_REG_TO_MEM:{
+		//XCHG [XXXX], AX
+		XCHG_MEM_REG:{
 			CODE:numToHexa[cmdCounter++],
 			FUNCTION: function(){
 				var temp;
-				registers.PS.step();
-				var to = memory[hexaToNum[registers.PS.get()]];
-				registers.PS.step();
-				var from = memory[hexaToNum[registers.PS.get()]];
-				temp = registers[to].get();
-				registers[to].set(memory[hexaToNum[from]]);
-				memory[hexaToNum[from]] = temp;
+				var to = getNextCommand();
+				var from = getNextCommand();
+				temp = memory[hexaToNum[to]];
+				memory[hexaToNum[to]] = registers[from].get();
+				registers[from].set(temp);
+				runNextInstruction();
 			}
 		},
-		XCHG_REG_TO_REG:{
+		//XCHG AX, AX
+		XCHG_REG_REG:{
 			CODE:numToHexa[cmdCounter++],
 			FUNCTION: function(){
 				var temp;
-				registers.PS.step();
-				var to = memory[hexaToNum[registers.PS.get()]];
-				registers.PS.step();
-				var from = memory[hexaToNum[registers.PS.get()]];
+				var to = getNextCommand();
+				var from = getNextCommand();
 				temp = registers[to].get(); 
 				registers[to].set(registers[from].get());
 				registers[from].set(temp);
+				runNextInstruction();
+			}
+		},
+		//ADD AX, XXXX
+		ADD_REG_VAL:{
+			CODE:numToHexa[cmdCounter++],
+			FUNCTION: function(){
+				var temp;
+				var to = getNextCommand();
+				var from = getNextCommand();
+				var regVal = registers[to].get(); 
+				var val = from;
+				var sum = hexaToNum[regVal]+hexaToNum[val];
+				if(sum > hexaToNum['FFFF']){
+					flags.CF = true;
+					sum = sum - hexaToNum['FFFF'];
+				}
+				registers[to].set(numToHexa[sum]);
+				runNextInstruction();
+			}
+		},
+		//ADD AX, AX
+		ADD_REG_REG:{
+			CODE:numToHexa[cmdCounter++],
+			FUNCTION: function(){
+				var temp;
+				var to = getNextCommand();
+				var from = getNextCommand();
+				var regVal = registers[to].get(); 
+				var val = registers[from].get();
+				var sum = hexaToNum[regVal]+hexaToNum[val];
+				if(sum > hexaToNum['FFFF']){
+					flags.CF = true;
+					sum = sum - hexaToNum['FFFF'];
+				}
+				registers[to].set(numToHexa[sum]);
+				runNextInstruction();
+			}
+		},
+		//ADC AX, XXXX
+		ADC_REG_VAL:{
+			CODE:numToHexa[cmdCounter++],
+			FUNCTION: function(){
+				var temp;
+				var to = getNextCommand();
+				var from = getNextCommand();
+				var regVal = registers[to].get(); 
+				var val = from;
+				var sum = hexaToNum[regVal]+hexaToNum[val] + (flags.CF?1:0);
+				if(sum > hexaToNum['FFFF']){
+					flags.CF = true;
+					sum = sum - hexaToNum['FFFF'];
+				}
+				registers[to].set(numToHexa[sum]);
+				runNextInstruction();
+			}
+		},
+		//ADC AX, AX
+		ADC_REG_REG:{
+			CODE:numToHexa[cmdCounter++],
+			FUNCTION: function(){
+				var temp;
+				var to = getNextCommand();
+				var from = getNextCommand();
+				var regVal = registers[to].get(); 
+				var val = registers[from].get();
+				var sum = hexaToNum[regVal]+hexaToNum[val] + (flags.CF?1:0);
+				if(sum > hexaToNum['FFFF']){
+					flags.CF = true;
+					sum = sum - hexaToNum['FFFF'];
+				}
+				registers[to].set(numToHexa[sum]);
+				runNextInstruction();
 			}
 		}
 	};
 	
-	/**
-	 * Translates The commands into an instructions array
-	 */
+	// Processor interruptions
+	//http://www.ctyme.com/intr/int.htm
+	interruptions = {
+		//DOS Interruptions
+		'0021':function(){
+			var al = registers.AL.get();
+			//http://spike.scu.edu.au/~barry/interrupts.html
+			var AH = {
+				//Terminate program
+				'0000':function(){throw 0;},
+				//Read char from std in and put it on AL
+				'0001':function(){
+					var c = getChar();
+					if(c == null){
+						registers.CS.stepBack();
+						registers.CS.stepBack();
+						window.setTimeout(function(){
+							runNextInstruction();
+						},10);
+					}else{
+						registers.AL.set(c.toString(16));
+						runNextInstruction();						
+					}
+				},
+				//Write char in DL on std out and copy it into AL
+				'0002':function(){
+					
+				}
+			};
+			AH[al]();
+		}
+	};
+	// Translates The commands into an instructions array for speed up
 	for(var cmd in commands){instructions[commands[cmd].CODE] = commands[cmd].FUNCTION;}
 	
-	/**
-	 * Compiler for MOV instruction
-	 */
-	compilers.MOV = function(expression){
-		//split arguments
-		var splited = expression.split(' ')[1].split(',');
-		var arg1 = splited[0].trim();
-		var arg2 = splited[1].trim();
-		var to, from;
-		if(isMemory(arg1)){
-			to = 'MEM';
-			arg1 = getMemoryAdress(arg1);
-		}else if(isRegister(arg1)) to = 'REG';
-		if(!to) throw new Error("Invalid argument: "+arg1);
-		if(isMemory(arg2)){
-			from = 'MEM';
-			arg2 = getMemoryAdress(arg2);
-		}else if(isRegister(arg2)) from = 'REG';
-		else if(isValue(arg2)) from = 'VAL';
-		if(!from) throw new Error("Invalid argument: "+arg2);
-		var instruction = commands['MOV_'+from+'_TO_'+to].CODE;
-		return [instruction, arg1, arg2];
-	};
-	
-	/**
-	 * Compiler for XCHG instruction
-	 */
-	compilers.XCHG = function(expression){
-		var splited = expression.split(' ')[1].split(',');
-		var arg1 = splited[0].trim();
-		var arg2 = splited[1].trim();
-		var op1, op2;
-		if(isMemory(arg1)){
-			op1 = 'MEM';
-			arg1 = getMemoryAdress(arg1);
-		}else if(isRegister(arg1)) op1 = 'REG';
-		if(!op1) throw new Error("Invalid argument: "+ arg1);
-		if(isMemory(arg2)){
-			op2 = 'MEM';
-			arg2 = getMemoryAdress(arg2);
-		}else if(isRegister(arg2)) op2 = 'REG';
-		if(!op2) throw new Error("Invalid argument: "+ arg2);
-		var instruction = commands['XCHG_'+op1+'_TO_'+op2].CODE;
-		return [instruction, arg1, arg2];
-	};
-	
-	/**
-	 * Setting functions to be accessed outside
-	 */
+	//Setting functions to be accessed outside
 	self.load = load;
 	self.run = run;
+	self.printRegisters = printRegisters;
+	window.onerror = function(e){
+		if(assembly.finish)assembly.finish(e);
+		assembly.printRegisters();
+	};
 })();
