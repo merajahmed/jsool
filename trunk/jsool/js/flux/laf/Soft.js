def5d3a68d76edf61c12e1d040d91c90ad5b5396
mikhas.rock <mikhas.rock@65f8f30c-6cf0-11de-b0b6-0bd851037a78>
2010-01-27 17:12:54 +0000

    - Created Container and fixed UIManager update after window resize

--- trunk/jsool/js/flux/laf/Soft.js
+++ trunk/jsool/js/flux/laf/Soft.js
@@ -13,9 +13,9 @@ js.flux.laf.Soft = (function(){
 	
 	var BODY_COLOR = 'rgb(223,234,248)';
 	
-	var BORDER_COLOR = 'rgb(223,234,248)';
-	var BORDER_RADIUS = 5;
-	var BORDER_WIDTH = 2;
+	var BORDER_COLOR = 'rgb(163,174,188)';
+	var BORDER_RADIUS = 2;
+	var BORDER_WIDTH = 1;
 	
 	var BORDER_FOCUS_COLOR = 'rgb(197,205,226)';
 	
@@ -39,6 +39,17 @@ js.flux.laf.Soft = (function(){
 			ctx.font = FONT_SIZE+'px '+FONT_FACE;
 			ctx.fillStyle = FONT_COLOR;
 			ctx.write(text,x+xCenter,y+yCenter,w);
+			
+			ctx.restore();
+		},
+		drawContainer: function(ctx, x, y, w, h){
+			ctx.save();
+			
+			ctx.fillStyle = BODY_COLOR;
+			ctx.strokeStyle = BORDER_COLOR;
+			ctx.lineWidth = BORDER_WIDTH;
+			ctx.fillRoundRect(x,y,w,h,BORDER_RADIUS);
+			ctx.strokeRoundRect(x,y,w,h,BORDER_RADIUS);
 			
 			ctx.restore();
 		}
