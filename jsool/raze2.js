9808aa3f2edb2c2c5b4c66593e3d8699f8b476c0
mikhas.rock <mikhas.rock@65f8f30c-6cf0-11de-b0b6-0bd851037a78>
2010-05-03 17:27:54 +0000

    - Optimizations on Raze2

--- jsool/raze2.js
+++ jsool/raze2.js
@@ -6,14 +6,15 @@
 	var qTIR= /^(#|\.)?([\w-\*]+)/, //Query Type Identifier Regexp
 		byIdRe = /^#([\w-]+)/,//is an ID?
 		byClassRe = /^\.([\w-]+)/,//is a Class?
-		byAttributeRe = /^\[([\w]+)(.*[=])?(.+)?]/,//is an Attribute?
+		byAttributeRe = /^\[([\w-]+)(.*[=])?(.+)?]/,//is an Attribute?
 		pseudoRe = /^:(\w+(-child)?)(\((.+)\))?/,//is it a Pseudo?
 		plainTagRe = /^([\w-]+)/,//is it a plain tag?
 		e="",
 		cache = {
 			filter:{},
 			selector:{}
-		};
+		},
+		counter=0;
 	//FUNCTIONS
 	var
 	fnGetNodes = "\nif(ctx.getElementsByTagName){ctx=ctx.getElementsByTagName(\"$\");}else{ns=[];cs;for(i=0,n;n=ctx[i++];){"+
@@ -55,15 +56,15 @@
 	};
 	
 	function compile(sel){
-		sel = sel.replace(/\[class=(\w*)]/g,'.$1');
-		sel = sel.replace(/\[id=(\w*)]/g,'#$1');
+		sel = sel.replace(/\[class=([\w\d-_]*)]/g,'.$1');
+		sel = sel.replace(/\[id=([\w\d-_]*)]/g,'#$1');
 		
 		var t = /[$]/g,
 		filters = sel.split(/\s+/),
 		o,
 		m,
 		fn,
-		s=["var q=function(ctx){var cs,ns,filtered,i,n,j,ch,r;"];
+		s=["var q=function raze2Query$",++counter,"(ctx){var cs,ns,f,i,n,j,ch,r,at;"];
 		
 		for(var i=0,f;f=filters[i++];){			
 			if(cache.filter[f]){
@@ -89,37 +90,39 @@
 				//CREATE FILTERS
 				if(f.length>0){
 					var before;
-					fn.push("{filtered=[];for(var a=0,el;el=ctx[a++];){if(");
+					fn.push("{f=[];for(var a=0,el;el=ctx[a++];){if(");
 					while(f.length>0){
 						before = f;
 						if((m=f.match(byIdRe))){
-							fn.push("el.id == \""+m[1]+"\"");
+							fn.push("el.id=== \""+m[1]+"\"");
 							f=f.replace(m[0],e);
 						}else if((m=f.match(byClassRe))){
 							fn.push("el.className.match(/\\b"+m[1]+"\\b/)");
 							f=f.replace(m[0],e);
-						}else if((m=f.match(plainTagRe))){
-							fn.push("el.tagName===\""+m[1].toUpperCase()+"\"");
-							f=f.replace(m[0],e);
-						}else if((m=f.match(byAttributeRe))){
+						}else
+						//if((m=f.match(plainTagRe))){
+						//	fn.push("el.tagName===\""+m[1].toUpperCase()+"\"");
+						//	f=f.replace(m[0],e);
+						//}else
+						if((m=f.match(byAttributeRe))){
 							var attr = m[1] == "class" ? "className" : m[1];
-							fn.push("(el[\""+attr+"\"]||el.getAttribute(\""+attr+"\"))");
+							fn.push("(at=(el[\""+attr+"\"]||el.getAttribute(\""+attr+"\")))");
 							if(m[2]&&m[3]){
 								switch(m[2]){
 								case"=":
-									fn.push(" && String(el[\""+attr+"\"])===\""+m[3]+"\"");
+									fn.push(" && String(at)===\""+m[3]+"\"");
 									break;
 								case"^=":
-									fn.push(" && el[\""+attr+"\"].substr(0,"+m[3].length+")===\""+m[3]+"\"");
+									fn.push(" && String(at).substr(0,"+m[3].length+")===\""+m[3]+"\"");
 									break;
 								case"$=":
-									fn.push(" && el[\""+attr+"\"].substr(0,el[\""+attr+"\"].length+"+m[3].length+") == \""+m[3]+"\"");
+									fn.push(" && String(at).substr(0,el[\""+attr+"\"].length+"+m[3].length+") == \""+m[3]+"\"");
 									break;
 								case"*=":
-									fn.push(" && el[\""+attr+"\"].indexOf(\""+m[3]+"\")!==-1");
+									fn.push(" && String(at).indexOf(\""+m[3]+"\")!==-1");
 									break;
 								default:
-									fn.push(" && el[\""+attr+"\"]"+m[2]+"\""+m[3]+"\"");
+									fn.push(" && String(at)"+m[2]+"\""+m[3]+"\"");
 									break;
 								}
 							}
@@ -133,7 +136,7 @@
 							throw new Error("Syntax error: "+f);
 						}
 					}
-					fn.push("true){filtered.push(el);}}ctx=filtered};");
+					fn.push("true){f.push(el);}}ctx=f};");
 				}
 				cache.filter[o]=fn.join(e);
 				s.push(cache.filter[o]);
@@ -175,14 +178,14 @@
 	}
 	
 	raze.query = query;
-	raze.compile = function(selector){
+	raze.compile = function compileSelector(selector){
 		if(!cache.selector[selector]){
 			cache.selector[selector] = compile(selector);
 		}
 		return cache.selector[selector];
 	};
 	
-	raze.queryNode = function(selector, context){
+	raze.queryNode = function queryNode(selector, context){
 		var res = query(selector, context);
 		return res[0] ? res[0] : null; 
 	};
