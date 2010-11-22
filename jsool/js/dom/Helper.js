8797b0ab9019f3a1866f7fe76813e27b1be127e1
mikhas.rock <mikhas.rock@65f8f30c-6cf0-11de-b0b6-0bd851037a78>
2010-11-22 19:59:04 +0000

    - js.dom.Helper using two different implementations to build/append DOM;

--- jsool/js/dom/Helper.js
+++ jsool/js/dom/Helper.js
@@ -4,7 +4,9 @@ js.dom.Helper = (function create_helper(){
 	var DOC = window.document,
 		EventManager = js.core.EventManager,
 		BODY,
-		proxy;
+		proxy,
+		specialAttributes = /^html|tag|children|text$/,
+		noCloseTags = /^input|br|hr|img$/;
 	
 	function init(){
 		proxy = DOC.getElementById("jsool-proxy");
@@ -33,21 +35,89 @@ js.dom.Helper = (function create_helper(){
 		
 		proxy.innerHTML = "";
 		BODY.removeChild(proxy);
-		return f.length == 1 ? f[0] : f;
+		return f;
 	}
 	
+	
+	function createHTML(def, b){
+		var atr,key,val;
+		b = b || [];
+		
+		if(String.isString(def)){
+			b.push(def);
+		}else if(Array.isArray(def)){
+			for(var i=0,el;el=def[i++];){
+				createHTML(el,b);
+			}
+		}else if(jsool.isObject(def)){
+			b.push('<');
+			def.tag = def.tag || 'div';
+			b.push(def.tag);
+			
+			for(atr in def){
+				val = def[atr];
+				
+				if(!specialAttributes.test(atr)){					
+					b.push(' ');
+					b.push(atr);
+					b.push('="');
+					if(typeof val === 'object'){
+						for(key in val){
+							b.push(key);
+							b.push(':');
+							b.push(val[key]);
+							b.push(';');
+						}
+					}else{
+						b.push(val);
+					}
+					b.push('"');
+				}
+			}
+			
+			if(noCloseTags.test(def.tag)){
+				b.push('/>');
+			}else{
+				b.push('>');
+				if(def.children){
+					createHTML(def.children,b);
+				}else if(def.html){
+					b.push(def.html);
+				}
+				b.push('</');
+				b.push(def.tag);
+				b.push('>');
+			}
+			
+			
+		}
+		
+		return b;
+	}
+	
+	
 	return{
-		createDom: function(html, parent){
-			var el;
+		createHTML: function(html, parent){
+			var html = createHTML(html).join('');
+			
+			if(parent){
+				parent = parent.dom ? parent.dom : parent;
+				parent.innerHTML += html;
+			}
+			return html;
+		},
+		append: function(parent, html){
+			var DH = js.dom.Helper;
+			parent = parent.dom ? parent.dom : parent;
+			
 			if(String.isString(html)){
-				el = userProxy(html);
-			}else if(Array.isArray(html)){
-				el = DOC.createDocumentFragment();
-				html.forEach(function(e){
-					js.dom.Helper.createDom(e, el);
+				parent.innerHTML += html;
+			}else if(jsool.isArray(html)){
+				Array.iterate(html,function(i,el){
+					DH.append(parent,el);
 				});
-			}else if(Object.isObject(html)){
-				el = DOC.createElement(html.tag || "div");
+			}else if(jsool.isObject(html)){
+				var el = DOC.createElement(html.tag || "div");				
 				
 				jsool.iterate(html,function(attr, val){
 					if(!(/(tag|style|html|children)/).test(attr)){
@@ -59,18 +129,14 @@ js.dom.Helper = (function create_helper(){
 					js.dom.Helper.applyStyle(el, html.style);
 				}
 				
+				parent.appendChild(el);
+				
 				if(html.children){
-					js.dom.Helper.createDom(html.children, el);
+					js.dom.Helper.append(el, html.children);
 				}else if(html.html){
 					el.innerHTML = html.html;
 				}
 			}
-			parent = parent || html.parent;
-			if(parent){
-				parent.append ? parent.append(el):parent.appendChild(el);
-			}
-			
-			return el;
 		},
 		applyStyle: function(el, style){
 			if(el.nodeType && style){
